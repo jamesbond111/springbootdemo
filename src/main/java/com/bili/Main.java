2380c69dd7a94c00798321f264d4e7b1644c2b40
jamesbond111 <21551067@zju.edu.cn>
2020-10-23 08:57:04 +0800

    asd

--- src/main/java/com/bili/Main.java
+++ src/main/java/com/bili/Main.java
@@ -1,6 +1,5 @@
 package com.bili;
 
-import org.apache.shardingsphere.api.sharding.standard.PreciseShardingValue;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -11,7 +10,7 @@ public class Main {
     //测试冲突1
     // 测试git commit -am
     //测试git commit -am --amend 111
-    //sdf 11
+    //sdf 111
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
     }
